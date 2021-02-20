7830ee71eae3ed8e8b4110abdae2a85f34274535
Oliver Berg <oliver@berg.moe>
2021-02-20 22:50:33 +0100

    [core] Add the 'ResourceUtils' file
    Signed-off-by: Oliver Berg <oliver@berg.moe>

---
+++ core/src/main/kotlin/krautils/io/ResourceUtils.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Oliver Berg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package krautils.io
+
+import krautils.KrautilsExperimental
+import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.toPath
+
+/**
+ * Returns a path to the resource with the given [name], or `null` if none is found.
+ *
+ * @see [ClassLoader.getResource]
+ */
+@ExperimentalPathApi
+@KrautilsExperimental
+public fun ClassLoader.getResourceAsPath(name: String): Path? = this.getResource(name)?.toURI()?.toPath()
