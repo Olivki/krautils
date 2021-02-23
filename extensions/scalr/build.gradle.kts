4894e96b4be97b8521ee00a03288a72945b35119
Oliver Berg <oliver@berg.moe>
2021-02-23 23:17:06 +0100

    Promote to stable version
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- extensions/scalr/build.gradle.kts
+++ extensions/scalr/build.gradle.kts
@@ -1,5 +1,5 @@
 description = "Kotlin extensions for the Java Scalr library."
-version = "1.0.0-SNAPSHOT" // TODO: change to '1.0.0'
+version = "1.0.0"
 
 dependencies {
     api(group = "org.imgscalr", name = "imgscalr-lib", version = "4.2")
