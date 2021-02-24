27371708232f44dd4c42342752a31baff897c043
Oliver Berg <oliver@berg.moe>
2021-02-24 16:08:26 +0100

    Promote 'commons-compress' to api level
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- extensions/compress/build.gradle.kts
+++ extensions/compress/build.gradle.kts
@@ -2,9 +2,7 @@ description = "Kotlin extensions for the Java Apache Commons Compress library."
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
-    implementation(group = "org.apache.commons", name = "commons-compress", version = "1.20")
-    // implementation group: 'com.github.luben', name: 'zstd-jni', version: '1.4.4-7'
+    api(group = "org.apache.commons", name = "commons-compress", version = "1.20")
     implementation(group = "com.github.luben", name = "zstd-jni", version = "1.4.4-7")
-    // implementation group: 'org.tukaani', name: 'xz', version: '1.8'
     implementation(group = "org.tukaani", name = "xz", version = "1.8")
 }
