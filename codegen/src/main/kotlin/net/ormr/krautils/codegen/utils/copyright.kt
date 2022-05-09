aa68da78a514369242b3151078e45279b9e6c530
Oliver Berg <oliver@berg.moe>
2022-05-09 17:06:52 +0200

    Update in code header
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- codegen/src/main/kotlin/net/ormr/krautils/codegen/utils/copyright.kt
+++ codegen/src/main/kotlin/net/ormr/krautils/codegen/utils/copyright.kt
@@ -19,7 +19,7 @@ package net.ormr.krautils.codegen.utils
 @JvmField
 val COPYRIGHT_HEADER = """
     |/*
-    | * Copyright 2021 Oliver Berg
+    | * Copyright 2021-2022 Oliver Berg
     | *
     | * Licensed under the Apache License, Version 2.0 (the "License");
     | * you may not use this file except in compliance with the License.
