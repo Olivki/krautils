169b2d6615047adc6a40a685aeac932b283da6b5
Oliver Berg <oliver@berg.moe>
2022-05-09 16:36:33 +0200

    Use 'me.him188.maven-central-publish' gradle plugin for maven publishing
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-import name.remal.gradle_plugins.dsl.extensions.convention
-import name.remal.gradle_plugins.dsl.extensions.get
-import name.remal.gradle_plugins.plugins.publish.ossrh.RepositoryHandlerOssrhExtension
-
 plugins {
+    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
     kotlin("jvm") version "1.5.10"
-    id("name.remal.maven-publish-ossrh") version "1.3.1"
-    id("name.remal.check-dependency-updates") version "1.3.1"
-    `maven-publish`
 }
 
 val kotestVersion: String by project
@@ -16,9 +10,8 @@ repositories {
 }
 
 subprojects {
+    apply(plugin = "me.him188.maven-central-publish")
     apply(plugin = "kotlin")
-    apply(plugin = "name.remal.maven-publish-ossrh")
-    apply(plugin = "maven-publish")
 
     project.group = "moe.kanon.krautils"
 
@@ -30,6 +23,12 @@ subprojects {
         testImplementation(group = "io.kotest", name = "kotest-runner-junit5", version = kotestVersion)
         testImplementation(group = "io.kotest", name = "kotest-assertions-core", version = kotestVersion)
         testImplementation(group = "io.kotest", name = "kotest-property", version = kotestVersion)
+    }
+
+    mavenCentralPublish {
+        useCentralS01()
+        singleDevGithubProject("Olivki", "krautils")
+        licenseApacheV2()
     }
 
     kotlin {
@@ -51,38 +50,5 @@ subprojects {
         test {
             useJUnitPlatform()
         }
-    }
-
-    afterEvaluate {
-        publishing.publications.withType<MavenPublication> {
-            pom {
-                name.set("${project.group}:${project.name}")
-                description.set(project.description)
-                url.set("https://github.com/Olivki/krautils")
-
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Olivki")
-                        name.set("Oliver Berg")
-                        email.set("oliver@berg.moe")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Olivki/krautils.git")
-                    developerConnection.set("scm:git:ssh://github.com/Olivki/krautils.git")
-                    url.set("https://github.com/Olivki/krautils")
-                }
-            }
-        }
-
-        publishing.repositories.convention[RepositoryHandlerOssrhExtension::class.java].ossrh()
     }
 }
